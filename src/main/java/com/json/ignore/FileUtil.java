bf538de714a86af9d4257ee5eb421f269e324383
rkonovalov <rkonovalov86@gmail.com>
2018-10-19 16:30:25 -0400

    Refactored

--- src/main/java/com/json/ignore/FileUtil.java
+++ src/main/java/com/json/ignore/FileUtil.java
@@ -1,33 +1,8 @@
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.json.ignore;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.json.ignore.filter.file.FileConfig;
-
 import java.io.*;
 import java.net.URL;
-
-/**
- * @author Ruslan {@literal <rkonovalov86@gmail.com>}
- * @version 1.0
- */
 
 public class FileUtil {
 
@@ -49,11 +24,10 @@ public class FileUtil {
 
             try {
                 String pathName = url.getFile();
-                try {
+                if(pathName != null) {
                     return new File(pathName);
-                } catch (NullPointerException e) {
-                    return null;
                 }
+
             } catch (NullPointerException e) {
                 return null;
             }
