bba7a49ae9e7059e97102263a885bb9f13abe987
rkonovalov <rkonovalov86@gmail.com>
2018-10-19 12:55:46 -0400

    Added unit test

--- src/test/java/com/json/ignore/filter/file/FileConfigTest.java
+++ src/test/java/com/json/ignore/filter/file/FileConfigTest.java
@@ -1,15 +1,50 @@
 package com.json.ignore.filter.file;
 
 import mock.MockClasses;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class FileConfigTest {
+    private FileConfig mockConfig;
+
+    @Before
+    public void init() {
+        mockConfig = MockClasses.getMockAdminFileConfig();
+        assertNotNull(mockConfig);
+    }
 
     @Test
-    public void testInitAdminFileConfig() {
-        FileConfig adminFileConfig = MockClasses.getMockAdminFileConfig();
-        assertNotNull(adminFileConfig);
+    public void testControllerClassName() {
+        FileConfig.Controller controller = mockConfig.getControllers().get(0);
+        assertEquals(controller.getClassName(), "com.json.ignore.filter.file.FileConfigTest");
+    }
+
+    @Test
+    public void testStrategy() {
+        FileConfig.Controller controller = mockConfig.getControllers().get(0);
+        FileConfig.Strategy strategy = controller.getStrategies().get(0);
+        assertEquals(strategy.getAttributeName(), "ROLE");
+        assertEquals(strategy.getAttributeValue(), "ADMIN");
+    }
+
+    @Test
+    public void testFilter() {
+        FileConfig.Controller controller = mockConfig.getControllers().get(0);
+        FileConfig.Strategy strategy = controller.getStrategies().get(0);
+        FileConfig.Filter filter = strategy.getFilters().get(0);
+        assertEquals(filter.getClassName(), "com.json.ignore.filter.file.FileConfigTest");
+    }
+
+    @Test
+    public void testField() {
+        FileConfig.Controller controller = mockConfig.getControllers().get(0);
+        FileConfig.Strategy strategy = controller.getStrategies().get(0);
+        FileConfig.Filter filter = strategy.getFilters().get(0);
+        FileConfig.Field field = filter.getFields().get(0);
+        assertEquals(field.getName(), "password");
     }
 }
