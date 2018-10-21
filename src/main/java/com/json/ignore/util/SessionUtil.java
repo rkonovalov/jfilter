19920c8c7b7f2755c215981134d5b4cedd37d14e
rkonovalov <rkonovalov86@gmail.com>
2018-10-21 18:09:39 -0400

    Refactored comments

--- src/main/java/com/json/ignore/util/SessionUtil.java
+++ src/main/java/com/json/ignore/util/SessionUtil.java
@@ -2,6 +2,7 @@ package com.json.ignore.util;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpRequest;
+
 import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
@@ -19,6 +20,7 @@ public final class SessionUtil {
      * Gets session from request
      * <p>
      * If request is instance of {@link ServletServerHttpRequest} returns session. Another way return null
+     *
      * @param serverHttpRequest {@link ServerHttpRequest} http request
      * @return {@link HttpSession} session, else null
      */
@@ -30,9 +32,31 @@ public final class SessionUtil {
             return null;
     }
 
-    public static boolean isSessionPropertyExists(HttpSession sessionParam, String attributeName, String attributeValue) {
-        if (sessionParam != null) {
-            Object sessionObject = sessionParam.getAttribute(attributeName);
+    /**
+     * Get attribute value from session attributes
+     * @param session {@link HttpSession} session
+     * @param attributeName {@link String} attribute name
+     * @return {@link Object} attribute value if exists, else null
+     */
+    public static Object getSessionProperty(HttpSession session, String attributeName) {
+        if (session != null) {
+            return session.getAttribute(attributeName);
+        } else
+            return null;
+    }
+
+    /**
+     * Check if session properties has property
+     * <p>
+     * Find property with name and value specified in method params
+     * @param session {@link HttpSession} session
+     * @param attributeName {@link String} attribute name
+     * @param attributeValue {@link String} expected attribute value
+     * @return {@link Boolean} true if property with name and value is exist, else false
+     */
+    public static boolean isSessionPropertyExists(HttpSession session, String attributeName, String attributeValue) {
+        if (session != null) {
+            Object sessionObject = getSessionProperty(session, attributeName);
             return Objects.equals(sessionObject, attributeValue);
         } else
             return false;
