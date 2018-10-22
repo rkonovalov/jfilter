83c720a3c8e69fc2f5d207a305e10298a51e1f05
rkonovalov <rkonovalov86@gmail.com>
2018-10-22 09:34:03 -0400

    Removed unused init

--- src/test/java/com/json/ignore/filter/BaseFilterTest.java
+++ src/test/java/com/json/ignore/filter/BaseFilterTest.java
@@ -1,25 +1,10 @@
 package com.json.ignore.filter;
 
-
 import com.json.ignore.filter.field.FieldFilter;
-import com.json.ignore.mock.MockHttpRequest;
-import com.json.ignore.mock.MockMethods;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.server.ServletServerHttpRequest;
 import static org.junit.Assert.*;
 
 public class BaseFilterTest {
-    private ServletServerHttpRequest request;
-    private MethodParameter methodParameter;
-
-    @Before
-    public void init() {
-        request = MockHttpRequest.getMockAdminRequest();
-        methodParameter = MockMethods.singleAnnotation();
-        assertNotNull(methodParameter);
-    }
 
     @Test(expected = NullPointerException.class)
     public void testNullMethod() {
