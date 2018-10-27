d01a34087d49476e4cbc7100f20f5b30d2e9a1d6
rkonovalov <rkonovalov86@gmail.com>
2018-10-26 23:13:59 -0400

    Refactored Reflect removed dirty code

--- src/main/java/com/json/ignore/reflect/Reflect.java
+++ src/main/java/com/json/ignore/reflect/Reflect.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Reflect {
-    private Map<Field, MethodRecord> fields;
+    private final Map<Field, MethodRecord> fields;
 
     public Reflect() {
         fields = new HashMap<>();
@@ -23,20 +23,4 @@ public class Reflect {
     public MethodRecord getRecord(Field field) {
         return fields.containsKey(field) ? fields.get(field) : addRecord(field);
     }
-
-
-    private void test() {
-        Field field = null;
-        Object object = null;
-
-        //1 loop to all declared methods and find only public, sort get and set
-        //Maybe 2 lists of getter/setter methods
-        //2 find equals and field
-
-        // Modifier.isPublic(method.getModifiers())
-
-        Object result = getRecord(field).getValue(() -> "Hello");
-        boolean result2 = getRecord(field).setValue(() -> new MethodEventValue(object, result));
-    }
-
 }
