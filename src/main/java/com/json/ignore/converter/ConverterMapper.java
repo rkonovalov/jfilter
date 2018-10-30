f5e12e06691fdcde051e7c5c6ceb3549d2e84d4f
rkonovalov <rkonovalov86@gmail.com>
2018-10-30 18:50:12 -0400

    Added comments to class

--- src/main/java/com/json/ignore/converter/ConverterMapper.java
+++ src/main/java/com/json/ignore/converter/ConverterMapper.java
@@ -3,26 +3,43 @@ package com.json.ignore.converter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.BeanSerializerFactory;
 import com.fasterxml.jackson.databind.ser.SerializerFactory;
-
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This is class represents additional functionality of standard ObjectMapper class
+ *
+ */
 public class ConverterMapper {
     private final ObjectMapper mapper;
     private final Map<Class, List<String>> ignoreList;
 
+    /**
+     * Constructor
+     * @param mapper  could be instance of {@link ObjectMapper} or {@link XmlMapper}
+     * @param ignoreList {@link Map} map of fields which could be ignored
+     */
     public ConverterMapper(ObjectMapper mapper, Map<Class, List<String>> ignoreList) {
         this.mapper = mapper;
         this.ignoreList = ignoreList;
         setIgnoreModifier();
     }
 
+    /**
+     * Creates instance of {@link SerializerFactory}  with configured withSerializerModifier value.
+     * Also see {@link ConverterMapperModifier}
+     */
     private void setIgnoreModifier() {
         SerializerFactory factory = BeanSerializerFactory.instance
                 .withSerializerModifier(new ConverterMapperModifier(ignoreList));
         mapper.setSerializerFactory(factory);
     }
 
+    /**
+     * Returns configured object mapper
+     * @return could be instance of {@link ObjectMapper} or {@link XmlMapper}
+     */
     public ObjectMapper getMapper() {
         return mapper;
     }
