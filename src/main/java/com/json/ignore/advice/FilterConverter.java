6f56fc2e254aca45809a6224691b48a18cc20a95
rkonovalov <rkonovalov86@gmail.com>
2018-10-30 14:02:25 -0400

    Refactored json/xml converters

---
+++ src/main/java/com/json/ignore/advice/FilterConverter.java
@@ -0,0 +1,56 @@
+package com.json.ignore.advice;
+
+import com.json.ignore.IgnoreMapper;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterConverter implements HttpMessageConverter<FilterClassWrapper> {
+    private final List<MediaType> supportedMedia;
+
+    protected FilterConverter() {
+        supportedMedia = new ArrayList<>();
+    }
+
+    public List<MediaType> getSupportedMedia() {
+        return supportedMedia;
+    }
+
+    @Override
+    public boolean canRead(Class aClass, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Class aClass, MediaType mediaType) {
+        return supportedMedia.indexOf(mediaType) >= 0;
+    }
+
+    @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return supportedMedia;
+    }
+
+    @Override
+    public FilterClassWrapper read(Class aClass, HttpInputMessage httpInputMessage) throws HttpMessageNotReadableException {
+        return null;
+    }
+
+    @Override
+    public void write(FilterClassWrapper object, MediaType mediaType, HttpOutputMessage httpOutputMessage) throws HttpMessageNotWritableException, IOException {
+        httpOutputMessage.getHeaders().setContentType(mediaType);
+        IgnoreMapper ignoreMapper = getIgnoreMapper(object, mediaType, httpOutputMessage);
+        httpOutputMessage.getBody().write(ignoreMapper.getMapper().writeValueAsBytes(object.getObject()));
+    }
+
+    protected IgnoreMapper getIgnoreMapper(FilterClassWrapper object, MediaType mediaType, HttpOutputMessage httpOutputMessage) {
+        return null;
+    }
+}
--- src/main/java/com/json/ignore/advice/FilterJsonConverter.java
+++ src/main/java/com/json/ignore/advice/FilterJsonConverter.java
@@ -2,52 +2,24 @@ package com.json.ignore.advice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.json.ignore.IgnoreMapper;
-import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
 
-public class FilterJsonConverter implements HttpMessageConverter<FilterClassWrapper> {
-    private final List<MediaType> supportedMedia;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+public class FilterJsonConverter extends FilterConverter {
 
     public FilterJsonConverter() {
-        supportedMedia = new ArrayList<>();
-        supportedMedia.add(MediaType.APPLICATION_JSON);
-        supportedMedia.add(new MediaType("application", "json", Charset.defaultCharset()));
-        supportedMedia.add(new MediaType("application", "*+json"));
+        getSupportedMediaTypes().addAll(Arrays.asList(
+                MediaType.APPLICATION_JSON,
+                new MediaType("application", "json", Charset.defaultCharset()),
+                new MediaType("application", "*+json")
+        ));
     }
 
     @Override
-    public boolean canRead(Class aClass, MediaType mediaType) {
-        return false;
-    }
-
-    @Override
-    public boolean canWrite(Class aClass, MediaType mediaType) {
-        return supportedMedia.indexOf(mediaType) >= 0;
-    }
-
-    @Override
-    public List<MediaType> getSupportedMediaTypes() {
-        return supportedMedia;
-    }
-
-    @Override
-    public FilterClassWrapper read(Class aClass, HttpInputMessage httpInputMessage) throws HttpMessageNotReadableException {
-        return null;
-    }
-
-    @Override
-    public void write(FilterClassWrapper object, MediaType mediaType, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
-        httpOutputMessage.getHeaders().setContentType(mediaType);
-
-        IgnoreMapper ignoreMapper = new IgnoreMapper(new ObjectMapper(), object.getIgnoreList());
-        httpOutputMessage.getBody().write(ignoreMapper.getMapper().writeValueAsBytes(object.getObject()));
+    protected IgnoreMapper getIgnoreMapper(FilterClassWrapper object, MediaType mediaType, HttpOutputMessage httpOutputMessage) {
+        return new IgnoreMapper(new ObjectMapper(), object.getIgnoreList());
     }
 }
