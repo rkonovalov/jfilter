cad16aaecbf574aa124935095a73758d0a82dfcb
rkonovalov <rkonovalov86@gmail.com>
2018-10-30 19:10:43 -0400

    Added comments for class

--- src/main/java/com/json/ignore/converter/ConverterMapperModifier.java
+++ src/main/java/com/json/ignore/converter/ConverterMapperModifier.java
@@ -4,23 +4,48 @@ import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
-
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class is  inherited from BeanSerializerModifier
+ *
+ * <p>
+ * Extends functionality of standard {@link BeanSerializerModifier} and
+ * give ability to filter ignorable fields from object
+ *
+ */
 public class ConverterMapperModifier extends BeanSerializerModifier {
     private final Map<Class, List<String>> ignoreList;
 
+    /**
+     * Constructor
+     * @param ignoreList {@link Map} list of ignorable field names
+     */
     public ConverterMapperModifier(Map<Class, List<String>> ignoreList) {
         this.ignoreList = ignoreList;
     }
 
+    /**
+     * Trying to change properties
+     *
+     * @param config serialization config
+     * @param beanDesc bean description
+     * @param beanProperties list of bean properties
+     * @return list of bean properties
+     */
     @Override
     public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription beanDesc, List<BeanPropertyWriter> beanProperties) {
         removeFields(beanDesc, beanProperties);
         return super.changeProperties(config, beanDesc, beanProperties);
     }
 
+    /**
+     * Search matches of field name in ignoreList and bean properties list
+     * If match is found then bean property will be removed from list
+     * @param clazz class
+     * @param beanProperties list of bean properties
+     */
     private void removeFieldsByClass(Class clazz, List<BeanPropertyWriter> beanProperties) {
         if(ignoreList.containsKey(clazz)) {
             List<String> ignores = ignoreList.get(clazz);
@@ -28,8 +53,21 @@ public class ConverterMapperModifier extends BeanSerializerModifier {
         }
     }
 
+    /**
+     * Attempt to remove bean property from bean properties list
+     *
+     * @param beanDesc bean description
+     * @param beanProperties list of bean properties
+     */
     private void removeFields(BeanDescription beanDesc, List<BeanPropertyWriter> beanProperties) {
+        /*
+         * Try to remove fields of specified class
+         */
         removeFieldsByClass(beanDesc.getType().getRawClass(), beanProperties);
+
+        /*
+         * Try to remove fields with not specified class
+         */
         removeFieldsByClass(null, beanProperties);
         removeFieldsByClass(void.class, beanProperties);
     }
