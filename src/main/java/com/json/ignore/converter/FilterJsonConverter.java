c7882e177ad65ec35584f2d2a6e8b7534ad98c80
rkonovalov <rkonovalov86@gmail.com>
2018-10-30 19:18:51 -0400

    Added comments for class

--- src/main/java/com/json/ignore/converter/FilterJsonConverter.java
+++ src/main/java/com/json/ignore/converter/FilterJsonConverter.java
@@ -7,8 +7,15 @@ import org.springframework.http.MediaType;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 
+/**
+ * This class represents of JSON serialization from response of Spring Web Service
+ */
 public class FilterJsonConverter extends FilterConverter {
 
+    /**
+     * Constructor
+     * Add supported media types
+     */
     public FilterJsonConverter() {
         getSupportedMediaTypes().addAll(Arrays.asList(
                 MediaType.APPLICATION_JSON,
@@ -17,8 +24,14 @@ public class FilterJsonConverter extends FilterConverter {
         ));
     }
 
+    /**
+     * Returns converter mapper
+     *
+     * @param object {@link FilterClassWrapper} wrapped object which should be serialized
+     * @return {@link ConverterMapper} converter mapper
+     */
     @Override
-    protected ConverterMapper getIgnoreMapper(FilterClassWrapper object, MediaType mediaType, HttpOutputMessage httpOutputMessage) {
+    protected ConverterMapper getIgnoreMapper(FilterClassWrapper object) {
         return new ConverterMapper(new ObjectMapper(), object.getIgnoreList());
     }
 }
