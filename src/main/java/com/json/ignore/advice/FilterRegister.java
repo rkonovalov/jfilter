d92b62b2b1c5ad48067dfba19b9af9ea85143705
rkonovalov <rkonovalov86@gmail.com>
2018-10-31 01:07:59 -0400

    Added comments for class

--- src/main/java/com/json/ignore/advice/FilterRegister.java
+++ src/main/java/com/json/ignore/advice/FilterRegister.java
@@ -1,5 +1,6 @@
 package com.json.ignore.advice;
 
+import com.json.ignore.EnableJsonFilter;
 import com.json.ignore.converter.FilterJsonConverter;
 import com.json.ignore.converter.FilterXmlConverter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 import java.util.List;
 
+/**
+ * This class used for register JSON/ and XML message converters
+ * Class depends from {@link EnableJsonFilter} annotation
+ */
 @Configuration
 public class FilterRegister extends WebMvcConfigurationSupport {
     private boolean enabled;
+
 
     @Autowired
     public void setWebApplicationContext(WebApplicationContext webApplicationContext) {
@@ -22,6 +28,10 @@ public class FilterRegister extends WebMvcConfigurationSupport {
         enabled = FilterProvider.isFilterEnabled(webApplicationContext);
     }
 
+    /**
+     * Add converters if {@link EnableJsonFilter} annotation is found in project
+     * @param converters list of {@link HttpMessageConverter}
+     */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         if (enabled) {
