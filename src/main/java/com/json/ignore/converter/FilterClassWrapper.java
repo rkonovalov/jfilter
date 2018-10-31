1e5f97b85626667dc7d60d215fd6c6d69a0a6796
rkonovalov <rkonovalov86@gmail.com>
2018-10-30 20:29:36 -0400

    Added comments for class

--- src/main/java/com/json/ignore/converter/FilterClassWrapper.java
+++ src/main/java/com/json/ignore/converter/FilterClassWrapper.java
@@ -1,13 +1,26 @@
 package com.json.ignore.converter;
 
+import com.json.ignore.advice.FilterAdvice;
+
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Class contains Serializable object and list of ignorable fields
+ * <p>
+ * Class used to send additional information from {@link FilterAdvice} to {@link FilterConverter}
+ *
+ */
 public class FilterClassWrapper implements Serializable {
     private final Serializable object;
     private final Map<Class, List<String>> ignoreList;
 
+    /**
+     * Constructor
+     * @param object Serializable object
+     * @param ignoreList ignorable fields
+     */
     public FilterClassWrapper(Serializable object, Map<Class, List<String>> ignoreList) {
         this.object = object;
         this.ignoreList = ignoreList;
