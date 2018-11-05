dddc84ac75cb09690c5439eac81d4b235eaac622
rkonovalov <rkonovalov86@gmail.com>
2018-11-05 17:18:51 -0500

    Added comments

--- src/main/java/com/json/ignore/advice/FileWatcherEvent.java
+++ src/main/java/com/json/ignore/advice/FileWatcherEvent.java
@@ -2,6 +2,15 @@ package com.json.ignore.advice;
 
 import java.io.File;
 
+/**
+ * The event that occurs when file is modified
+ */
 public interface FileWatcherEvent {
+
+    /**
+     * Event occurs if file modified
+     *
+     * @param file modified file
+     */
     void onEvent(File file);
 }
