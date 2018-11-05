fc046b1a1b164126637df28b4e0acfdc3da238dd
rkonovalov <rkonovalov86@gmail.com>
2018-11-05 10:18:32 -0500

    Added integration test

--- src/test/java/com/json/ignore/advice/FileWatcherITTest.java
+++ src/test/java/com/json/ignore/advice/FileWatcherITTest.java
@@ -10,33 +10,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import java.io.File;
+import java.util.Date;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @ContextConfiguration(classes = WSConfigurationEnabled.class)
 @RunWith(SpringRunner.class)
 @WebAppConfiguration("src/main/resources")
 public class FileWatcherITTest {
+    private File file;
     @Autowired
     private FileWatcher fileWatcher;
 
     @Before
     public void init() {
         Assert.assertNotNull(fileWatcher);
+        file = FileFilter.resourceFile("config.xml");
+
+        assertNotNull(file);
     }
 
     @Test
     public void testAdd() {
-        boolean result = fileWatcher.add(FileFilter.resourceFile("config.xml"), f -> System.out.println(f.getAbsoluteFile()));
+        boolean result = fileWatcher.add(file, f -> System.out.println(f.getAbsoluteFile()));
         assertTrue(result);
     }
 
     @Test
     public void testAddTwice() {
-        fileWatcher.add(FileFilter.resourceFile("config.xml"), f -> System.out.println(f.getAbsoluteFile()));
+        fileWatcher.add(file, f -> System.out.println(f.getAbsoluteFile()));
 
-        boolean result = fileWatcher.add(FileFilter.resourceFile("config.xml"), f -> System.out.println(f.getAbsoluteFile()));
+        boolean result = fileWatcher.add(file, f -> System.out.println(f.getAbsoluteFile()));
         assertTrue(result);
     }
 
@@ -44,5 +51,23 @@ public class FileWatcherITTest {
     public void testUnExistFile() {
         boolean result = fileWatcher.add(FileFilter.resourceFile("unexist_config.xml"), f -> System.out.println(f.getAbsoluteFile()));
         assertFalse(result);
+    }
+
+    @Test
+    public void testFileIsModifiedFalse() {
+        boolean result = fileWatcher.fileIsModified(file.getAbsolutePath());
+        assertFalse(result);
+    }
+
+    @Test
+    public void testFileIsModifiedTrue() {
+        boolean added = fileWatcher.add(file, f -> System.out.println(f.getAbsoluteFile()));
+        assertTrue(added);
+
+        boolean modified = file.setLastModified(new Date().getTime());
+        assertTrue(modified);
+
+        boolean result = fileWatcher.fileIsModified(file.getAbsolutePath());
+        assertTrue(result);
     }
 }
