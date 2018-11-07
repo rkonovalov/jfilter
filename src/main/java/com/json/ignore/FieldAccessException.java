36910f850e17c3295faa01ef9d3deac03c4f0b23
rkonovalov <rkonovalov86@gmail.com>
2018-11-07 17:54:09 -0500

    Refactored exception

--- src/main/java/com/json/ignore/FieldAccessException.java
+++ src/main/java/com/json/ignore/FieldAccessException.java
@@ -1,10 +1,21 @@
 package com.json.ignore;
 
+import com.sun.istack.internal.Nullable;
+
 /**
  * Exception will throw on {@link IllegalAccessException}
  */
 public class FieldAccessException extends RuntimeException {
+
     public FieldAccessException(Throwable cause) {
        super(cause);
+    }
+
+    public FieldAccessException(String msg) {
+        super(msg);
+    }
+
+    public FieldAccessException(@Nullable String msg, @Nullable Throwable cause) {
+        super(msg, cause);
     }
 }
