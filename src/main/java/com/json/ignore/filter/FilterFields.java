260874edd3a892cdc6e53fa38a873b59d39dadfa
rkonovalov <rkonovalov86@gmail.com>
2018-11-09 12:32:15 -0500

    Added javadoc comments

--- src/main/java/com/json/ignore/filter/FilterFields.java
+++ src/main/java/com/json/ignore/filter/FilterFields.java
@@ -3,15 +3,29 @@ package com.json.ignore.filter;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * Class used in filters
+ *
+ * <p>This class contains map of fields which could be filtered
+ */
 public class FilterFields implements Serializable {
     private static final long serialVersionUID = -2354837314560228182L;
 
     private final Map<Class, List<String>> fieldsMap;
 
+    /**
+     * Creates a new instance of the {@link FilterFields} class.
+     */
     public FilterFields() {
         this.fieldsMap = new HashMap<>();
     }
 
+    /**
+     * Creates a new instance of the {@link FilterFields} class.
+     *
+     * @param className class name
+     * @param fields list of field name
+     */
     public FilterFields(Class className, List<String> fields) {
         this();
         this.fieldsMap.put(className, fields);
@@ -21,14 +35,30 @@ public class FilterFields implements Serializable {
         return fieldsMap;
     }
 
+    /**
+     * Returns filterable field names of defined class
+     *
+     * @param className class name
+     * @return list of fields, if defined class not found returns empty array list
+     */
     public List<String> getFields(Class className) {
         return fieldsMap.containsKey(className) ? fieldsMap.get(className) : new ArrayList<>();
     }
 
-    public void appendToMap(Class classname, List<String> value) {
-        List<String> fields = fieldsMap.computeIfAbsent(classname, k -> new ArrayList<>());
-        value.forEach(v -> {
-            if (!fields.contains(v)) fields.add(v);
+    /**
+     * Append filterable fields in map
+     *
+     * <p>Method attempt to find defined class name in map.
+     * If class is found then adds filterable fields in exist list, else
+     * creates new array list and adds fields in it
+     *
+     * @param classname class name
+     * @param fields list of filterable fields
+     */
+    public void appendToMap(Class classname, List<String> fields) {
+        List<String> foundFields = fieldsMap.computeIfAbsent(classname, k -> new ArrayList<>());
+        fields.forEach(v -> {
+            if (!foundFields.contains(v)) foundFields.add(v);
         });
     }
 
