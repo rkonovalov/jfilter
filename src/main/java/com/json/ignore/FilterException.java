69c83e00e5081b82965ec38c93c89c615f12978f
rkonovalov <rkonovalov86@gmail.com>
2018-11-09 12:52:09 -0500

    Renamed exception name

--- src/main/java/com/json/ignore/FilterException.java
+++ src/main/java/com/json/ignore/FilterException.java
@@ -3,11 +3,11 @@ package com.json.ignore;
 /**
  * Exception will throw on {@link IllegalAccessException}
  */
-public class FieldAccessException extends RuntimeException {
+public class FilterException extends RuntimeException {
 
     private static final long serialVersionUID = -1580361832535921569L;
 
-    public FieldAccessException(Throwable cause) {
+    public FilterException(Throwable cause) {
         super(cause);
     }
 
