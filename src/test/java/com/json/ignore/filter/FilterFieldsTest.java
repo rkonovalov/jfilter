ffa049d4b6c200ff6d1905e398d368764bba7cdc
rkonovalov <rkonovalov86@gmail.com>
2018-11-09 08:17:53 -0500

    Added unit tests for test hashCode

--- src/test/java/com/json/ignore/filter/FilterFieldsTest.java
+++ src/test/java/com/json/ignore/filter/FilterFieldsTest.java
@@ -1,7 +1,7 @@
 package com.json.ignore.filter;
 
 import org.junit.Test;
-
+import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class FilterFieldsTest {
@@ -21,5 +21,23 @@ public class FilterFieldsTest {
         boolean result = filterFields.equals(1);
 
         assertFalse(result);
+    }
+
+    @Test
+    public void testHashCodeEquals() {
+        FilterFields filterFields = new FilterFields(String.class, Arrays.asList("id", "password"));
+        FilterFields filterFields2 = new FilterFields(String.class, Arrays.asList("id", "password"));
+
+        assertNotSame(filterFields, filterFields2);
+        assertEquals(filterFields.hashCode(), filterFields2.hashCode());
+    }
+
+    @Test
+    public void testHashCodeNotEquals() {
+        FilterFields filterFields = new FilterFields(String.class, Arrays.asList("id", "password"));
+        FilterFields filterFields2 = new FilterFields(String.class, Arrays.asList("some_id", "some_password"));
+
+        assertNotSame(filterFields, filterFields2);
+        assertNotEquals(filterFields.hashCode(), filterFields2.hashCode());
     }
 }
