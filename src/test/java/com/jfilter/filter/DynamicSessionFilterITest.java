607511fd806810d0bf9ece056218bb54d55579dc
rkonovalov <rkonovalov86@gmail.com>
2018-11-10 17:29:33 -0500

    Refactored tests

--- src/test/java/com/jfilter/filter/DynamicSessionFilterITest.java
+++ src/test/java/com/jfilter/filter/DynamicSessionFilterITest.java
@@ -3,22 +3,34 @@ package com.jfilter.filter;
 import com.jfilter.components.DynamicFilterProvider;
 import com.jfilter.mock.MockHttpRequest;
 import com.jfilter.mock.MockMethods;
+import com.jfilter.mock.MockUser;
 import com.jfilter.mock.config.WSConfigurationEnabled;
 import com.jfilter.request.RequestSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runner.notification.RunListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @ContextConfiguration(classes = WSConfigurationEnabled.class)
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration("src/main/resources")
+@RunListener.ThreadSafe
 public class DynamicSessionFilterITest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     private DynamicFilterProvider dynamicFilterProvider;
@@ -36,10 +48,11 @@ public class DynamicSessionFilterITest {
     public void testMockDynamicNullFilter() {
         MethodParameter methodParameter = MockMethods.mockDynamicNullFilter();
 
-        RequestSession requestSession = new RequestSession(MockHttpRequest.getMockDynamicFilterRequest(null));
+        RequestSession requestSession = new RequestSession(MockHttpRequest.getMockDynamicFilterRequest(new FilterFields(MockUser.class, Arrays.asList("id", "password"))));
 
+        dynamicFilterProvider.findDynamicFilters();
         FilterFields found = dynamicFilterProvider.getFields(methodParameter, requestSession);
 
-        assertNotNull(found);
+        assertNull(found);
     }
 }
