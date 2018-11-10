fe75ddb5901111ed17083bc1a8a61dd3b14a90c5
rkonovalov <rkonovalov86@gmail.com>
2018-11-10 18:21:31 -0500

    Removed unused import

--- src/test/java/com/jfilter/components/FilterAdviceITest.java
+++ src/test/java/com/jfilter/components/FilterAdviceITest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
