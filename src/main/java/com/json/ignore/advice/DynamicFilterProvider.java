99c4e11d601b3fbe3fb2ec7ad3505bc5f4931967
rkonovalov <rkonovalov86@gmail.com>
2018-11-10 00:12:57 -0500

    Test commit

--- src/main/java/com/json/ignore/advice/DynamicFilterProvider.java
+++ src/main/java/com/json/ignore/advice/DynamicFilterProvider.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
