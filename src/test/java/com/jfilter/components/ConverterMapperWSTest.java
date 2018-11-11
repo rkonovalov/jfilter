ec944596a2a1790bde8a9f0acf0ef32291fb3502
rkonovalov <rkonovalov86@gmail.com>
2018-11-10 21:38:13 -0500

    Removed unused imports

--- src/test/java/com/jfilter/components/ConverterMapperWSTest.java
+++ src/test/java/com/jfilter/components/ConverterMapperWSTest.java
@@ -3,24 +3,18 @@ package com.jfilter.components;
 import com.jfilter.filter.FilterFields;
 import com.jfilter.mock.MockClasses;
 import com.jfilter.mock.MockHttpRequest;
-import com.jfilter.mock.MockMethods;
 import com.jfilter.mock.MockUser;
 import com.jfilter.mock.config.WSConfigurationEnabled;
-import com.jfilter.request.RequestSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import java.util.Arrays;
-
 import static com.jfilter.components.DynamicSessionFilter.ATTRIBUTE_FILTER_FIELDS;
 import static com.jfilter.mock.webservice.WSMethod.MAPPING_SIGN_IN_DYNAMIC;
 import static org.junit.Assert.assertEquals;
