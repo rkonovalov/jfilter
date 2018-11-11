fa579befeafd278c487706781c9a50bddab82f04
rkonovalov <rkonovalov86@gmail.com>
2018-11-10 22:22:36 -0500

    Removed unused annotations

--- src/test/java/com/jfilter/mock/config/WSConfigurationEnabled.java
+++ src/test/java/com/jfilter/mock/config/WSConfigurationEnabled.java
@@ -2,26 +2,18 @@ package com.jfilter.mock.config;
 
 import com.jfilter.EnableJsonFilter;
 import com.jfilter.components.FilterRegister;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 import java.util.List;
 
-@SuppressWarnings("ALL")
-@RunWith(SpringRunner.class)
 @Configuration
 @ComponentScan({"com.jfilter", "com.jfilter.components", "com.jfilter.mock.webservice"})
-
 @EnableJsonFilter
-@Ignore
 public class WSConfigurationEnabled extends WebMvcConfigurerAdapter {
     @Autowired
     private FilterRegister filterRegister;
