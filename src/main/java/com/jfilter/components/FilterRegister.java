4d3496453fc6c25e37d3a55440671415d297eb59
rkonovalov <rkonovalov86@gmail.com>
2019-04-12 11:36:27 -0400

    Added additional defaultJsonConverter and defaultXmlConverter used in FilterJsonConverter and FilterXmlConverter constructors

--- src/main/java/com/jfilter/components/FilterRegister.java
+++ src/main/java/com/jfilter/components/FilterRegister.java
@@ -1,11 +1,14 @@
 package com.jfilter.components;
 
+import com.jfilter.ConverterUtil;
 import com.jfilter.EnableJsonFilter;
 import com.jfilter.converter.FilterXmlConverter;
 import com.jfilter.converter.FilterJsonConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -20,6 +23,9 @@ import java.util.List;
 public class FilterRegister extends WebMvcConfigurationSupport {
     private boolean enabled;
 
+    private MappingJackson2HttpMessageConverter defaultJsonConverter;
+    private MappingJackson2XmlHttpMessageConverter defaultXmlConverter;
+
 
     @Autowired
     public void setWebApplicationContext(WebApplicationContext webApplicationContext) {
@@ -27,7 +33,12 @@ public class FilterRegister extends WebMvcConfigurationSupport {
          * Important! For enabling filtration, should be specified one of application bean with EnableJsonFilter annotation
          */
         enabled = FilterProvider.isFilterEnabled(webApplicationContext);
+
+        defaultJsonConverter = ConverterUtil.getDefaultJsonConverter(webApplicationContext);
+        defaultXmlConverter = ConverterUtil.getDefaultXmlConverter(webApplicationContext);
     }
+
+
 
     /**
      * Add converters if {@link EnableJsonFilter} annotation is found in project
@@ -36,8 +47,8 @@ public class FilterRegister extends WebMvcConfigurationSupport {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         if (enabled) {
-            converters.add(new FilterJsonConverter());
-            converters.add(new FilterXmlConverter());
+            converters.add(new FilterJsonConverter(defaultJsonConverter));
+            converters.add(new FilterXmlConverter(defaultXmlConverter));
         }
         super.configureMessageConverters(converters);
     }
