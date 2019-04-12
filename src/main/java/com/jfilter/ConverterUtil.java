6d99c7b5647005e1eba7d5dbb5798b22f516475c
rkonovalov <rkonovalov86@gmail.com>
2019-04-12 11:32:18 -0400

    Added ConverterUtil class

---
+++ src/main/java/com/jfilter/ConverterUtil.java
@@ -0,0 +1,25 @@
+package com.jfilter;
+
+import org.springframework.beans.BeansException;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.context.WebApplicationContext;
+
+public class ConverterUtil {
+
+    private static <T> T getBean(WebApplicationContext webApplicationContext, Class<T> tClass) {
+        try {
+            return webApplicationContext.getBean(tClass);
+        } catch (BeansException e) {
+            return null;
+        }
+    }
+
+    public static MappingJackson2HttpMessageConverter getDefaultJsonConverter(WebApplicationContext webApplicationContext) {
+        return getBean(webApplicationContext, MappingJackson2HttpMessageConverter.class);
+    }
+
+    public static MappingJackson2XmlHttpMessageConverter getDefaultXmlConverter(WebApplicationContext webApplicationContext) {
+        return getBean(webApplicationContext, MappingJackson2XmlHttpMessageConverter.class);
+    }
+}
