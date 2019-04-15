2fd335b68592c171fd7d50c0a4c4f87bf336c74b
rkonovalov <rkonovalov86@gmail.com>
2019-04-15 09:18:42 -0400

    Changed from extending class WebMvcConfigurationSupport to implementing WebMvcConfigurer because of issues with Spring Boot, Swagger and LocalDateTime type of fields

--- src/main/java/com/jfilter/components/FilterRegister.java
+++ src/main/java/com/jfilter/components/FilterRegister.java
@@ -6,11 +6,18 @@ import com.jfilter.converter.FilterXmlConverter;
 import com.jfilter.converter.FilterJsonConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +27,10 @@ import java.util.List;
  * <p>Class depends from {@link EnableJsonFilter} annotation
  */
 @Configuration
-public class FilterRegister extends WebMvcConfigurationSupport {
+public class FilterRegister implements WebMvcConfigurer {
     private boolean enabled;
-
     private MappingJackson2HttpMessageConverter defaultJsonConverter;
     private MappingJackson2XmlHttpMessageConverter defaultXmlConverter;
-
 
     @Autowired
     public void setWebApplicationContext(WebApplicationContext webApplicationContext) {
@@ -38,8 +43,6 @@ public class FilterRegister extends WebMvcConfigurationSupport {
         defaultXmlConverter = ConverterUtil.getDefaultXmlConverter(webApplicationContext);
     }
 
-
-
     /**
      * Add converters if {@link EnableJsonFilter} annotation is found in project
      * @param converters list of {@link HttpMessageConverter}
@@ -50,6 +53,90 @@ public class FilterRegister extends WebMvcConfigurationSupport {
             converters.add(new FilterJsonConverter(defaultJsonConverter));
             converters.add(new FilterXmlConverter(defaultXmlConverter));
         }
-        super.configureMessageConverters(converters);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+
+    }
+
+    @Override
+    public Validator getValidator() {
+        return null;
+    }
+
+    @Override
+    public MessageCodesResolver getMessageCodesResolver() {
+        return null;
     }
 }
