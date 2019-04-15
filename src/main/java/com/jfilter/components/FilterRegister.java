d8d2a070e60e9949437635037764975b3e86fad2
rkonovalov <rkonovalov86@gmail.com>
2019-04-15 11:21:19 -0400

    Removed unused field

--- src/main/java/com/jfilter/components/FilterRegister.java
+++ src/main/java/com/jfilter/components/FilterRegister.java
@@ -33,12 +33,8 @@ public class FilterRegister implements WebMvcConfigurer {
     private MappingJackson2XmlHttpMessageConverter defaultXmlConverter;
     private ObjectMapper objectMapper;
 
-    private final WebApplicationContext webApplicationContext;
-
     @Autowired
     public FilterRegister(WebApplicationContext webApplicationContext) {
-        this.webApplicationContext = webApplicationContext;
-
         /*
          * Important! For enabling filtration, should be specified one of application bean with EnableJsonFilter annotation
          */
