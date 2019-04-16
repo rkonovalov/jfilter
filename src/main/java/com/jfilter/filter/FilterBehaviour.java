d6370d80e8720339563d83615c687a2246984024
rkonovalov <rkonovalov86@gmail.com>
2019-04-16 12:29:27 -0400

    Added FilterBehaviour Case #7

---
+++ src/main/java/com/jfilter/filter/FilterBehaviour.java
@@ -0,0 +1,14 @@
+package com.jfilter.filter;
+
+public enum FilterBehaviour {
+    /**
+     * If this option selected, JFilter will hide selected fields.
+     */
+    HIDE_FIELDS,
+
+
+    /**
+     * If this option selected, JFilter will hide all other fields but not selected ones.
+     */
+    KEEP_FIELDS
+}
