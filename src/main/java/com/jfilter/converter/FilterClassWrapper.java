b87a74cafe9b0ceb860e3f28a053bbefe08312a7
rkonovalov <rkonovalov86@gmail.com>
2019-04-19 12:33:51 -0400

    Changed parameter in constructor

--- src/main/java/com/jfilter/converter/FilterClassWrapper.java
+++ src/main/java/com/jfilter/converter/FilterClassWrapper.java
@@ -1,36 +1,50 @@
 package com.jfilter.converter;
 
 import com.jfilter.components.FilterAdvice;
-import com.jfilter.filter.FilterFields;
+import com.jfilter.components.FilterConverter;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 
 import java.io.Serializable;
 
 /**
- * Class contains Serializable object and list of ignorable fields
+ * Class contains Serializable object and list of fields which should be kept or hidden
  *
  * <p>Class used to send additional information from {@link FilterAdvice} to {@link FilterConverter}
  */
 public class FilterClassWrapper implements Serializable {
     private static final long serialVersionUID = -6250969684778521840L;
     private final Serializable object;
-    private final FilterFields filterFields;
+    private MethodParameterDetails methodParameterDetails;
 
     /**
      * Creates a new instance of the {@link FilterClassWrapper} class.
      *
-     * @param object Serializable object
-     * @param filterFields ignorable fields
+     * @param object                 Serializable object
+     * @param methodParameterDetails objectMapper item
      */
-    public FilterClassWrapper(Serializable object, FilterFields filterFields) {
+    public FilterClassWrapper(Serializable object, MethodParameterDetails methodParameterDetails) {
         this.object = object;
-        this.filterFields = filterFields;
+        this.methodParameterDetails = methodParameterDetails;
     }
 
+    /**
+     * Serializable object retrieved from {@link FilterAdvice#beforeBodyWrite(Serializable, MethodParameter, MediaType, Class, ServerHttpRequest, ServerHttpResponse)}
+     *
+     * @return {@link Serializable}
+     */
     public Serializable getObject() {
         return object;
     }
 
-    public FilterFields getIgnoreList() {
-        return filterFields;
+    /**
+     * MethodParameter details retrieved from {@link FilterAdvice#beforeBodyWrite(Serializable, MethodParameter, MediaType, Class, ServerHttpRequest, ServerHttpResponse)}
+     *
+     * @return {@link MethodParameterDetails}
+     */
+    public MethodParameterDetails getMethodParameterDetails() {
+        return methodParameterDetails;
     }
 }
