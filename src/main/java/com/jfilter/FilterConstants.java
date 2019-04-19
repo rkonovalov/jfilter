afdba67f338c315fb860a143417983d0963bc7c4
rkonovalov <rkonovalov86@gmail.com>
2019-04-19 13:36:45 -0400

    Changed constructor

--- src/main/java/com/jfilter/FilterConstants.java
+++ src/main/java/com/jfilter/FilterConstants.java
@@ -5,13 +5,13 @@ package com.jfilter;
  */
 public class FilterConstants {
 
-    private FilterConstants() {
-        throw new IllegalStateException("FilterConstants class");
-    }
-
     public static final String MEDIA_TYPE_APPLICATION = "application";
     public static final String MEDIA_SUB_TYPE_JSON = "json";
     public static final String MEDIA_SUB_TYPE_JSON2 = "*+json";
     public static final String MEDIA_SUB_TYPE_XML = "xml";
     public static final String MEDIA_SUB_TYPE_XML2 = "*+xml";
+
+    private FilterConstants() {
+        throw new IllegalStateException("FilterConstants class");
+    }
 }
