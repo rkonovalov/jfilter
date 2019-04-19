1cdd1830ea5a3bb747d28752d786b4d11785ec1a
rkonovalov <rkonovalov86@gmail.com>
2019-04-19 13:21:06 -0400

    Added constructor

--- src/main/java/com/jfilter/FilterConstants.java
+++ src/main/java/com/jfilter/FilterConstants.java
@@ -4,6 +4,11 @@ package com.jfilter;
  * Filter constants class
  */
 public class FilterConstants {
+
+    private FilterConstants() {
+        throw new IllegalStateException("FilterConstants class");
+    }
+
     public static final String MEDIA_TYPE_APPLICATION = "application";
     public static final String MEDIA_SUB_TYPE_JSON = "json";
     public static final String MEDIA_SUB_TYPE_JSON2 = "*+json";
