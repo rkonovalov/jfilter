096a2ddafd338d214cc6c01e0e4895ce5ca47d44
rkonovalov <rkonovalov86@gmail.com>
2019-04-19 13:07:23 -0400

    Added constants class

---
+++ src/main/java/com/jfilter/FilterConstants.java
@@ -0,0 +1,12 @@
+package com.jfilter;
+
+/**
+ * Filter constants class
+ */
+public class FilterConstants {
+    public static final String MEDIA_TYPE_APPLICATION = "application";
+    public static final String MEDIA_SUB_TYPE_JSON = "json";
+    public static final String MEDIA_SUB_TYPE_JSON2 = "*+json";
+    public static final String MEDIA_SUB_TYPE_XML = "xml";
+    public static final String MEDIA_SUB_TYPE_XML2 = "*+xml";
+}
