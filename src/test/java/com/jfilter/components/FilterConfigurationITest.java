b6eced19ae5abd7da7eeebd36990df45f5c69550
rkonovalov <rkonovalov86@gmail.com>
2019-04-20 18:20:22 -0400

    Refactored FilterConfiguration tests

--- src/test/java/com/jfilter/components/FilterConfigurationITest.java
+++ src/test/java/com/jfilter/components/FilterConfigurationITest.java
@@ -7,31 +7,22 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 @Component
 public class FilterConfigurationITest {
     private FilterConfiguration filterConfiguration;
-    private MockMvc mockMvc;
-
-    @Autowired
-    public void setWebApplicationContext(WebApplicationContext webApplicationContext) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
-
-    @Before
-    public void init() throws Exception {
-        WSConfiguration.instance(WSConfiguration.Instance.FILTER_ENABLED2, this);
-    }
 
     @Autowired
     public FilterConfigurationITest setFilterConfiguration(FilterConfiguration filterConfiguration) {
         this.filterConfiguration = filterConfiguration;
         return this;
+    }
+
+    @Before
+    public void init() throws Exception {
+        WSConfiguration.instance(WSConfiguration.Instance.FILTER_ENABLED2, this);
     }
 
     @Test
