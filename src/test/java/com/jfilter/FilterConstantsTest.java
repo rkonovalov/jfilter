6d8152e23cc3128b8cef0fc04ef2980754daeb26
rkonovalov <rkonovalov86@gmail.com>
2019-04-21 09:05:53 -0400

    Added FilterConstants test

---
+++ src/test/java/com/jfilter/FilterConstantsTest.java
@@ -0,0 +1,16 @@
+package com.jfilter;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class FilterConstantsTest {
+
+    @Test(expected = InvocationTargetException.class)
+    public void constructorTest() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Constructor<FilterConstants> constructor = FilterConstants.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        FilterConstants filterConstants = constructor.newInstance();
+    }
+}
