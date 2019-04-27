2e54099e9f0797c8c2f0bd6b8e20325690bce7d6
rkonovalov <rkonovalov86@gmail.com>
2019-04-27 14:54:51 -0400

    Refactored FilterConverter for extending AbstractHttpMessageConverter Changed write object method

--- src/main/java/com/jfilter/components/FilterConverter.java
+++ src/main/java/com/jfilter/components/FilterConverter.java
@@ -1,97 +1,53 @@
 package com.jfilter.components;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.*;
 import com.jfilter.converter.FilterClassWrapper;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.http.converter.*;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * This class intercepts all responses of Spring Web Service which returns objects instance FilterClassWrapper
- */
-public class FilterConverter implements HttpMessageConverter<Object> {
+public class FilterConverter extends AbstractHttpMessageConverter<Object> {
     private FilterConfiguration filterConfiguration;
 
-    /**
-     * Creates a new instance of the {@link FilterConverter} class
-     *
-     * @param filterConfiguration {@link FilterConfiguration}
-     */
     public FilterConverter(FilterConfiguration filterConfiguration) {
         this.filterConfiguration = filterConfiguration;
     }
 
-    /**
-     * Returns ability to deserialize.
-     * This functionality is disabled in current converter
-     *
-     * @param aClass    class name
-     * @param mediaType {@link MediaType} media type
-     * @return true if converter supports to deserialize object instance of aClass
-     */
     @Override
-    public boolean canRead(Class<?> aClass, MediaType mediaType) {
-        return false;
+    protected boolean supports(Class<?> clazz) {
+        return Serializable.class == clazz;
     }
 
-    /**
-     * Returns ability to serialize.
-     * Attempt to find supported media type in supported media  type list of configuration
-     *
-     * @param aClass    class name
-     * @param mediaType {@link MediaType} media type
-     * @return true if converter supports to serialize object instance of aClass and specified media type, else false
-     */
-    @Override
-    public boolean canWrite(Class<?> aClass, MediaType mediaType) {
-        return filterConfiguration.isEnabled() &&
-                (filterConfiguration.supportedMediaTypes().indexOf(mediaType) >= 0 || Objects.isNull(mediaType));
-    }
-
-    /**
-     * List of supported media types from configuration
-     *
-     * @return {@link List} and {@link MediaType}
-     */
     @Override
     public List<MediaType> getSupportedMediaTypes() {
         return filterConfiguration.supportedMediaTypes();
     }
 
-    /**
-     * Deserialize object from HttpInputMessage.
-     * This functionality is disabled in current converter
-     *
-     * @param aClass           class name
-     * @param httpInputMessage {@link HttpInputMessage}
-     * @return {@link Serializable}
-     * @throws HttpMessageNotReadableException exception
-     */
     @Override
-    public FilterClassWrapper read(Class<?> aClass, HttpInputMessage httpInputMessage) throws HttpMessageNotReadableException {
+    public boolean canRead(Class clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Class clazz, MediaType mediaType) {
+        return filterConfiguration.isEnabled() &&
+                (filterConfiguration.supportedMediaTypes().indexOf(mediaType) >= 0 || Objects.isNull(mediaType));
+    }
+
+    @Override
+    protected Object readInternal(Class<?> clazz, HttpInputMessage inputMessage) throws HttpMessageNotReadableException {
         return null;
     }
 
-    /**
-     * Serialize FilterClassWrapper
-     *
-     * @param object            instance of {@link Serializable}
-     * @param mediaType         {@link MediaType}
-     * @param httpOutputMessage HttpOutputMessage
-     * @throws HttpMessageNotWritableException exception
-     * @throws IOException                     exception
-     */
     @Override
-    public void write(Object object, MediaType mediaType, HttpOutputMessage httpOutputMessage) throws HttpMessageNotWritableException, IOException {
-        httpOutputMessage.getHeaders().setContentType(mediaType);
+    protected void writeInternal(Object object, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+        MediaType contentType = outputMessage.getHeaders().getContentType();
 
         //If object is FilterClassWrapper try to serialize object using filters(if configured)
         if (object instanceof FilterClassWrapper) {
@@ -102,11 +58,11 @@ public class FilterConverter implements HttpMessageConverter<Object> {
                     .findObjectMapper(wrapper.getMethodParameterDetails());
 
             //Serialize object with ObjectMapper
-            httpOutputMessage.getBody().write(objectMapper.writeValueAsBytes(wrapper.getObject()));
+            objectMapper.writeValue(outputMessage.getBody(), wrapper.getObject());
         } else {
             //Otherwise try to serialize object without filters by default ObjectMapper from filterConfiguration
-            ObjectMapper objectMapper = filterConfiguration.getMapper(mediaType);
-            httpOutputMessage.getBody().write(objectMapper.writeValueAsBytes(object));
+            ObjectMapper objectMapper = filterConfiguration.getMapper(contentType);
+            objectMapper.writeValue(outputMessage.getBody(), object);
         }
     }
 }
