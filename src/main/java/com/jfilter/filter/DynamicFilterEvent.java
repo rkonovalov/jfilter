2faf2c9f105c05523fe9b6ca46b534696484a6ee
rkonovalov <rkonovalov86@gmail.com>
2019-05-03 22:00:30 -0400

    Deprecated method onGetFilterFields Added onRequest method instead of onGetFilterFields

--- src/main/java/com/jfilter/filter/DynamicFilterEvent.java
+++ src/main/java/com/jfilter/filter/DynamicFilterEvent.java
@@ -1,5 +1,6 @@
 package com.jfilter.filter;
 
+import com.comparator.Comparator;
 import com.jfilter.components.DynamicFilterProvider;
 import com.jfilter.request.RequestSession;
 import org.springframework.core.MethodParameter;
@@ -11,5 +12,25 @@ import org.springframework.core.MethodParameter;
  * get {@link FilterFields} from inherited class
  */
 public interface DynamicFilterEvent {
-    FilterFields onGetFilterFields(MethodParameter methodParameter, RequestSession request);
+
+    /**
+     * Deprecated since 1.0.14 version, use onRequest instead
+     *
+     * @deprecated use {@link #onRequest(Comparator)} instead.
+     *
+     * @param methodParameter {@link MethodParameter}
+     * @param request {@link RequestSession}
+     * @return instance of {@link FilterFields}
+     */
+    @Deprecated
+    default FilterFields onGetFilterFields(MethodParameter methodParameter, RequestSession request){
+        throw new UnsupportedOperationException("Deprecated, use onRequest instead");
+    }
+
+    /**
+     * Request event handler
+     *
+     * @param comparator {@link Comparator} comparator object which will be used by {@link DynamicFilterProvider} for retrieve {@link FilterFields}
+     */
+    void onRequest(Comparator<RequestSession, FilterFields> comparator);
 }
