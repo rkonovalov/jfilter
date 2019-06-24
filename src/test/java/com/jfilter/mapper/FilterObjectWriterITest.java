f86d8d973358ede33dccb948db0832cddee41313
rkonovalov <rkonovalov86@gmail.com>
2019-06-24 13:47:45 -0400

    Added tests

--- src/test/java/com/jfilter/mapper/FilterObjectWriterITest.java
+++ src/test/java/com/jfilter/mapper/FilterObjectWriterITest.java
@@ -2,14 +2,20 @@ package com.jfilter.mapper;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.jfilter.components.FilterConfiguration;
+import com.jfilter.components.FilterProvider;
+import com.jfilter.converter.FilterClassWrapper;
+import com.jfilter.converter.MethodParameterDetails;
+import com.jfilter.filter.BaseFilter;
+import com.jfilter.mock.MockMethods;
 import com.jfilter.mock.MockUser;
 import com.jfilter.mock.config.WSConfigurationEnabled;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -23,30 +29,61 @@ import static org.junit.Assert.*;
 @WebAppConfiguration
 public class FilterObjectWriterITest {
     private FilterObjectMapper objectMapper;
-    private ObjectWriter writer;
+    private FilterObjectWriter writer;
     private MockUser user;
+    private ByteArrayOutputStream out;
+    private JsonEncoding encoding;
+    private JsonGenerator generator;
 
     @Autowired
     private FilterConfiguration filterConfiguration;
 
-    @Before
-    public void init() {
-        objectMapper = new FilterObjectMapper(filterConfiguration);
-        writer = objectMapper.writer();
-        user = new MockUser();
-    }
+    @Autowired
+    private FilterProvider filterProvider;
 
-    @Test
-    public void testWriter() {
-        assertTrue(writer instanceof FilterObjectWriter);
+    @Before
+    public void init() throws IOException {
+        objectMapper = new FilterObjectMapper(filterConfiguration);
+        writer = (FilterObjectWriter) objectMapper.writer();
+        user = new MockUser();
+
+        out = new ByteArrayOutputStream();
+        encoding = JsonEncoding.UTF8;
+        generator = this.objectMapper.getFactory().createGenerator(out, encoding);
     }
 
     @Test
     public void testWriteValue() throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        JsonEncoding encoding = JsonEncoding.UTF8;
-        JsonGenerator generator = this.objectMapper.getFactory().createGenerator(out, encoding);
         writer.writeValue(generator, user);
         assertNotNull(out.toString());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testWriteValueFilterClassWrapperNull() throws IOException {
+        FilterClassWrapper filterClassWrapper = new FilterClassWrapper(user, null);
+        writer.writeValue(generator, filterClassWrapper);
+    }
+
+    @Test
+    public void testWriteValueFilterClassWrapperNotNull() throws IOException {
+
+        MethodParameter methodParameter = MockMethods.singleAnnotation();
+
+        BaseFilter filter = filterProvider.getFilter(methodParameter);
+
+        MethodParameterDetails methodParameterDetails = new MethodParameterDetails(methodParameter,
+                MediaType.APPLICATION_JSON, filter.getFields(user, null));
+
+        FilterClassWrapper filterClassWrapper = new FilterClassWrapper(user, methodParameterDetails);
+        writer.writeValue(generator, filterClassWrapper);
+
+        String output = out.toString();
+
+        assertEquals(-1, output.indexOf("\"id\""));
+    }
+
+    @Test
+    public void testGetFilterConfiguration() {
+        assertEquals(filterConfiguration, writer.getFilterConfiguration());
     }
 }
