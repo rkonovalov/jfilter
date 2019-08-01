dac0fba8d3a2f9b9aa549f0ccc21b554ae873016
rkonovalov <rkonovalov86@gmail.com>
2019-07-31 21:38:33 -0400

    Added test of getMethodParameter method

--- src/test/java/com/jfilter/converter/MethodParameterDetailsTest.java
+++ src/test/java/com/jfilter/converter/MethodParameterDetailsTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class MethodParameterDetailsTest {
 
     private MethodParameter methodParameter;
@@ -22,75 +25,81 @@ public class MethodParameterDetailsTest {
     @Test
     public void testGetMethodHashCode() {
         MethodParameterDetails methodParameterDetails = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, new FilterFields());
-        Assert.assertEquals(methodParameterHash, methodParameterDetails.getMethodHashCode());
+        assertEquals(methodParameterHash, methodParameterDetails.getMethodHashCode());
     }
 
     @Test
     public void testEquals() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, new FilterFields());
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, new FilterFields());
-        Assert.assertEquals(methodParameterDetails1, methodParameterDetails2);
+        assertEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testEqualsCurrentMethod() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, new FilterFields());
-        Assert.assertEquals(methodParameterDetails1, methodParameterDetails1);
+        assertEquals(methodParameterDetails1, methodParameterDetails1);
     }
 
     @Test
     public void testNotEquals() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, new FilterFields());
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_XML, null);
-        Assert.assertNotEquals(methodParameterDetails1, methodParameterDetails2);
+        assertNotEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testNullNotEquals() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, new FilterFields());
-        Assert.assertNotEquals(methodParameterDetails1, null);
+        assertNotEquals(methodParameterDetails1, null);
     }
 
     @Test
     public void testEqualsHashCode() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, null, null);
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, null, null);
-        Assert.assertEquals(methodParameterDetails1, methodParameterDetails2);
+        assertEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testNotEqualsHashCode() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(MockMethods.dynamicSessionFilter(), null, null);
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, null, null);
-        Assert.assertNotEquals(methodParameterDetails1, methodParameterDetails2);
+        assertNotEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testEqualsMediaType() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, null);
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, null);
-        Assert.assertEquals(methodParameterDetails1, methodParameterDetails2);
+        assertEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testNotEqualsMediaType() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_JSON, null);
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, MediaType.APPLICATION_XML, null);
-        Assert.assertNotEquals(methodParameterDetails1, methodParameterDetails2);
+        assertNotEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testEqualsFilterFields() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, null, new FilterFields());
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, null, new FilterFields());
-        Assert.assertEquals(methodParameterDetails1, methodParameterDetails2);
+        assertEquals(methodParameterDetails1, methodParameterDetails2);
     }
 
     @Test
     public void testNotEqualsFilterFields() {
         MethodParameterDetails methodParameterDetails1 = new MethodParameterDetails(methodParameter, null, new FilterFields());
         MethodParameterDetails methodParameterDetails2 = new MethodParameterDetails(methodParameter, null, null);
-        Assert.assertNotEquals(methodParameterDetails1, methodParameterDetails2);
+        assertNotEquals(methodParameterDetails1, methodParameterDetails2);
+    }
+
+    @Test
+    public void testGetMethodParameter() {
+        MethodParameterDetails methodParameterDetails = new MethodParameterDetails(methodParameter, null, new FilterFields());
+        assertEquals(methodParameterDetails.getMethodParameter(), methodParameter);
     }
 
 
