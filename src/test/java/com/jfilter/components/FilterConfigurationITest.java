9e5e2a5eecee7851b3ad450d82908b31df0e775b
rkonovalov <rkonovalov86@gmail.com>
2019-07-31 20:20:17 -0400

    Removed unused import

--- src/test/java/com/jfilter/components/FilterConfigurationITest.java
+++ src/test/java/com/jfilter/components/FilterConfigurationITest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
