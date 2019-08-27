410dc0f95592a717be509b91836eea84b82d30ca
rkonovalov <rkonovalov86@gmail.com>
2019-08-26 21:56:02 -0400

    Added javadoc comments

--- src/main/java/com/jfilter/util/FilterUtil.java
+++ src/main/java/com/jfilter/util/FilterUtil.java
@@ -6,12 +6,31 @@ import com.jfilter.filter.FilterFields;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+/**
+ * Filter utils class
+ */
 public class FilterUtil {
 
+    private FilterUtil() {
+        throw new IllegalStateException("FilterUtil class");
+    }
+
+    /**
+     * Sets filterFields object into http request ATTRIBUTE_FILTER_FIELDS attribute
+     *
+     * @param request {@link HttpServletRequest} http request
+     * @param filterFields {@link FilterFields} instance of FilterFields
+     */
     public static void useFilter(HttpServletRequest request, FilterFields filterFields) {
         request.setAttribute(DynamicSessionFilter.ATTRIBUTE_FILTER_FIELDS, filterFields);
     }
 
+    /**
+     * Sets filterFields object into http session ATTRIBUTE_FILTER_FIELDS attribute
+     *
+     * @param session {@link HttpSession} http session
+     * @param filterFields {@link FilterFields} instance of FilterFields
+     */
     public static void useFilter(HttpSession session, FilterFields filterFields) {
         session.setAttribute(DynamicSessionFilter.ATTRIBUTE_FILTER_FIELDS, filterFields);
     }
