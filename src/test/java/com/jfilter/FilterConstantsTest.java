f55c7abb8f15fa2862607a2a464e6eeb371ecb39
rkonovalov <rkonovalov86@gmail.com>
2019-08-26 22:36:59 -0400

    Refactored constructorTest method

--- src/test/java/com/jfilter/FilterConstantsTest.java
+++ src/test/java/com/jfilter/FilterConstantsTest.java
@@ -1,16 +1,12 @@
 package com.jfilter;
 
 import org.junit.Test;
-
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class FilterConstantsTest {
 
     @Test(expected = InvocationTargetException.class)
     public void constructorTest() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        Constructor<FilterConstants> constructor = FilterConstants.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        constructor.newInstance();
+        TestUtils.instantiateObject(FilterConstants.class);
     }
 }
