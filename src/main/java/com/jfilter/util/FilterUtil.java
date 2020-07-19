73a28e92afb05ef95613a7535072f00df2acf9b8
ruslan <rkonovalov86@yahoo.com>
2020-07-18 23:03:36 -0400

    Added Javadoc comments

--- src/main/java/com/jfilter/util/FilterUtil.java
+++ src/main/java/com/jfilter/util/FilterUtil.java
@@ -4,6 +4,7 @@ import com.jfilter.components.DynamicSessionFilter;
 import com.jfilter.filter.DynamicFilter;
 import com.jfilter.filter.FilterFields;
 import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -39,11 +40,27 @@ public class FilterUtil {
         session.setAttribute(DynamicSessionFilter.ATTRIBUTE_FILTER_FIELDS, filterFields);
     }
 
+    /**
+     * Extract DynamicFilter from class annotated with  DynamicFilter annotation and  containing current method
+     *
+     * @param methodParameter method which contains in class annotated with {@link RestController}
+     * @param annotationClass class inherited from {@link DynamicFilter}
+     * @param <T> generic of {@link DynamicFilter}
+     * @return object inherited from {@link DynamicFilter}
+     */
     @SuppressWarnings("unchecked")
     public static <T> T getClassDeclaredAnnotation(MethodParameter methodParameter, Class<? extends DynamicFilter> annotationClass) {
         return (T) methodParameter.getContainingClass().getDeclaredAnnotation(annotationClass);
     }
 
+    /**
+     * Extract DynamicFilter from method annotated with DynamicFilter annotation
+     *
+     * @param methodParameter method which contains in class annotated with {@link RestController}
+     * @param annotationClass class inherited from {@link DynamicFilter}
+     * @param <T> generic of {@link DynamicFilter}
+     * @return object inherited from {@link DynamicFilter}
+     */
     @SuppressWarnings("unchecked")
     public static <T> T getMethodDeclaredAnnotation(MethodParameter methodParameter, Class<? extends DynamicFilter> annotationClass) {
         return (T) methodParameter.getMethod().getDeclaredAnnotation(annotationClass);
